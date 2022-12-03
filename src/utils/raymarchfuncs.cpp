d4b9f06504e15ef0678e3982fe214f65aa5db68c
William Sun <william.h.sun.88@gmail.com>
2022-12-03 16:25:38 -0500

    update var names

--- src/utils/raymarchfuncs.cpp
+++ src/utils/raymarchfuncs.cpp
@@ -1,1 +1,8 @@
 #include "raymarchfuncs.h"
+
+float sceneSDF(glm::vec3 worldSpacePoint, const RayTraceScene& scene) {
+    const std::vector<Shape*>& shapes = scene.getShapes();
+
+    for(const Shape* shape : shapes){
+    }
+}
