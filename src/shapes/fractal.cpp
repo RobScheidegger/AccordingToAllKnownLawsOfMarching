753698560e72a8a3373a185b3150d72cb06eaf22
Anh Truong <truongdinhanh09@gmail.com>
2022-12-08 17:32:48 -0500

    fixed mandelbulb sdf

--- src/shapes/fractal.cpp
+++ src/shapes/fractal.cpp
@@ -12,26 +12,39 @@ float Fractal::mandelbulbSDF(glm::vec4 p) const {
     glm::vec4 trap = glm::vec4(abs(w),m);
     float dz = 1.0;
 
-    for( int i=0; i< 4; i++ ) {
-        dz = 8.0*pow(m,3.5)*dz + 1.0;
+    for( int i=0; i<20; i++ )
+    {
+        // polynomial version (no trigonometrics, but MUCH slower)
+        float m2 = m*m;
+        float m4 = m2*m2;
+        dz = 8.0*sqrt(m4*m2*m)*dz + 1.0;
 
-        // z = z^8+c
-        float r = length(w);
-        float b = 8.0*acos( w.y/r);
-        float a = 8.0*glm::atan( w[0], w[1] );
-        w = glm::vec3(p) + powf(r, 8.f) * glm::vec3( sin(b)*sin(a), cos(b), sin(b)*cos(a) );
+        float x = w.x; float x2 = x*x; float x4 = x2*x2;
+        float y = w.y; float y2 = y*y; float y4 = y2*y2;
+        float z = w.z; float z2 = z*z; float z4 = z2*z2;
+
+        float k3 = x2 + z2;
+        float k2 = glm::inversesqrt( k3*k3*k3*k3*k3*k3*k3 );
+        float k1 = x4 + y4 + z4 - 6.0*y2*z2 - 6.0*x2*y2 + 2.0*z2*x2;
+        float k4 = x2 - y2 + z2;
+
+        w.x = p.x +  64.0*x*y*z*(x2-z2)*k4*(x4-6.0*x2*z2+z4)*k1*k2;
+        w.y = p.y + -16.0*y2*k3*k4*k4 + k1*k1;
+        w.z = p.z +  -8.0*y*k4*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*k1*k2;
 
         trap = min( trap, glm::vec4(abs(w),m) );
 
         m = dot(w,w);
-        if( m > 256.0 ) {
+        if( m > 256.0 )
             break;
-        }
-
     }
+
+    glm::vec4 resColor = glm::vec4(m, glm::vec3(trap[1], trap[2], trap[3]));
+
     // distance estimation (through the Hubbard-Douady potential)
     return 0.25*log(m)*sqrt(m)/dz;
 }
+
 
 glm::vec4 sphere (glm::vec4 z) {
   float r2 = glm::dot(z.xyz(), z.xyz());
